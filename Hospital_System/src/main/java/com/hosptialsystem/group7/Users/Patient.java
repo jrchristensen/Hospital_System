5cd2fcdf00fee1ca8fb044bb0326bb35381f3745
jrchristensen <jacob-r-christensen@uiowa.edu>
2015-03-23 13:44:29 -0500

    Added patient and providers class

---
+++ Hospital_System/src/main/java/com/hosptialsystem/group7/Users/Patient.java
@@ -0,0 +1,18 @@
+package com.hosptialsystem.group7.Users;
+
+/**
+ * @author Jacob
+ *
+ */
+public class Patient extends User {
+
+	/**
+	 * @param applicantEmail
+	 * @param applicantPassword
+	 */
+	public Patient(String applicantEmail, String applicantPassword) {
+		super(applicantEmail, applicantPassword);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Hospital_System/src/main/java/com/hosptialsystem/group7/Users/Providers.java
@@ -0,0 +1,19 @@
+
+package com.hosptialsystem.group7.Users;
+
+/**
+ * @author Jacob
+ *
+ */
+public class Providers extends User {
+
+	/**
+	 * @param applicantEmail
+	 * @param applicantPassword
+	 */
+	public Providers(String applicantEmail, String applicantPassword) {
+		super(applicantEmail, applicantPassword);
+		
+	}
+
+}
